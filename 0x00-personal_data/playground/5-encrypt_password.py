39e1c0d565f041d729d31252d0f35b1c706d438c
greenbel <obisann@gmail.com>
2023-05-25 11:03:07 +0000

    0x00-T6: init

--- 0x00-personal_data/playground/5-encrypt_password.py
+++ 0x00-personal_data/playground/5-encrypt_password.py
@@ -14,9 +14,3 @@ def hash_password(password: str) -> bytes:
         bytes: a bytes string representing the hash.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-
-
-def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks if @hashed_password is the hash of @password.
-    """
-    return bcrypt.checkpw(password.encode(), hashed_password)
