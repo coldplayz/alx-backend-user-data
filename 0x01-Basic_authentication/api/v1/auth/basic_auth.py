b503e381d598518495de8a15d0f30d2989c878bb
greenbel <obisann@gmail.com>
2023-05-29 20:23:12 +0000

    0x01-T12: init

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -70,7 +70,9 @@ class BasicAuth(Auth):
             return (None, None)
 
         # credential separator xter in decoded_base64_authorization_header
-        return tuple(decoded_base64_authorization_header.split(':'))
+        return tuple(
+                decoded_base64_authorization_header.split(':', maxsplit=1),
+                )
 
     def user_object_from_credentials(
             self,
