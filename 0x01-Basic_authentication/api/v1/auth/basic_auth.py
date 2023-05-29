c72636aac4dba89a9ec9f0898930def56991b212
greenbel <obisann@gmail.com>
2023-05-29 17:42:20 +0000

    0x01-T9: init

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -51,3 +51,21 @@ class BasicAuth(Auth):
                     ).decode('utf-8')
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+            ) -> (str, str):
+        """Returns the user email and password from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None or\
+                type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+
+        # decode_base64_authorization_header is a string
+        if ':' not in decoded_base64_authorization_header:
+            # no credential separator character
+            return (None, None)
+
+        # credential separator xter in decoded_base64_authorization_header
+        return tuple(decoded_base64_authorization_header.split(':'))
